76ba6398437360ac7770b6e0c135a8495c48b690
SAWA-25 <3227527084@qq.com>
2026-06-11 05:49:48 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import { OSProvider } from './context/OSContext';
+import { MusicProvider } from './context/MusicContext';
+import PhoneShell from './components/PhoneShell';
+import BuildBadge from './components/BuildBadge';
+import DevDebugPanel from './components/DevDebugPanel';
+import VRBroadcast from './components/VRBroadcast';
+import { isIOSStandaloneWebApp } from './utils/iosStandalone';
+
+const App: React.FC = () => {
+  const useAbsoluteShell = typeof window !== 'undefined' && isIOSStandaloneWebApp();
+  const shellClassName = useAbsoluteShell
+    ? 'fixed inset-0 w-full h-full bg-transparent overflow-hidden'
+    : 'relative w-full bg-transparent overflow-hidden';
+  const shellStyle = useAbsoluteShell
+    ? { height: 'var(--app-height, 100lvh)', minHeight: 'var(--app-height, 100lvh)' }
+    : { height: 'var(--app-height, 100lvh)', minHeight: 'var(--app-height, 100lvh)' };
+
+  return (
+    <>
+      <div
+        className={shellClassName}
+        style={shellStyle}
+      >
+        <div
+          className={`${useAbsoluteShell ? 'absolute' : 'fixed'} inset-0 w-full h-full z-0 bg-transparent`}
+          style={{ transform: 'translateZ(0)' }}
+        >
+          <OSProvider>
+            <MusicProvider>
+              <PhoneShell />
+            </MusicProvider>
+          </OSProvider>
+        </div>
+      </div>
+      <BuildBadge />
+      <DevDebugPanel />
+      <VRBroadcast />
+    </>
+  );
+};
+
+export default App;
